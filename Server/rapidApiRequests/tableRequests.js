034f65b9e7972c4610cddc056881cbf307b39121
OrAmram100 <63103357+OrAmram100@users.noreply.github.com>
2023-09-07 17:14:38 +0300

    rapid api table request function

--- Server/rapidApiRequests/tableRequests.js
+++ Server/rapidApiRequests/tableRequests.js
@@ -1,6 +1,26 @@
+const axios = require('axios');
 
 const getTableData = async () => {
-    return {};
+
+    const options = {
+    method: 'GET',
+    url: 'https://api-football-v1.p.rapidapi.com/v3/standings',
+    params: {
+        season: '2023',
+        league: '39'
+    },
+    headers: {
+        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+        'X-RapidAPI-Host': process.env.RPID_API_HOST
+    }
+    };
+
+    try {
+        const response = await axios.request(options);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 module.exports = {getTableData};
--- Server/routes/tables.js
+++ Server/routes/tables.js
@@ -9,7 +9,6 @@ const router = require("express").Router();
 //GET TABLES DATA
 
 router.get("/getTables", async(req,res)=>{
-    console.log("object")
     const tablesData = await tableRequests.getTableData();
     res.status(200).send(tablesData);
 
