f9c9970441753db2f815d81b426c7985f3293c50
Talevin12 <talevin2000@gmail.com>
2023-09-08 11:33:31 +0300

    Get players info and stats by team

--- Server/rapidApiRequests/playersRequests.js
+++ Server/rapidApiRequests/playersRequests.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 
 const getPlayersByTeam = async (teamId) => {
-
     const options = {
         method: 'GET',
         url: 'https://api-football-v1.p.rapidapi.com/v3/players/squads',
-        params: { team: teamId },
+        params: {
+            team: teamId,
+        },
         headers: {
             'X-RapidAPI-Key': process.env.RAPID_API_KEY,
             'X-RapidAPI-Host': process.env.RPID_API_HOST
@@ -20,4 +21,27 @@ const getPlayersByTeam = async (teamId) => {
     }
 };
 
-module.exports = { getPlayersByTeam };
+const getPlayersInfoAndStatsByTeam = async (teamId) => {
+    const options = {
+        method: 'GET',
+        url: 'https://api-football-v1.p.rapidapi.com/v3/players',
+        params: {
+            team: teamId,
+            league: process.env.EPL_LEAGUE_ID,
+            season: process.env.CURRENT_SEASON
+        },
+        headers: {
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+            'X-RapidAPI-Host': process.env.RPID_API_HOST
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = { getPlayersByTeam, getPlayersInfoAndStatsByTeam };
