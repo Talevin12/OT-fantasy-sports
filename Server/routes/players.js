bcc3c784b5f1f16e586701efc3eafc4c0c7909f7
Talevin12 <talevin2000@gmail.com>
2023-09-08 11:33:31 +0300

    Get players info and stats by team

--- Server/routes/players.js
+++ Server/routes/players.js
@@ -8,4 +8,11 @@ router.get("/getPlayersByTeam", async (req, res) => {
     res.status(200).send(PlayersByTeamData);
 })
 
+//GET PLAYERS INFO AND STATS BY TEAM DATA
+router.get("/getPlayersInfoAndStatsByTeam", async (req, res) => {
+    let { teamId } = req.query;
+    const PlayersInfoAndStatsByTeamData = await playersRequests.getPlayersInfoAndStatsByTeam(teamId);
+    res.status(200).send(PlayersInfoAndStatsByTeamData);
+})
+
 module.exports = router;
