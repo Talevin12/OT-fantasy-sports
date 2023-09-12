d8a87e8ae3bd904af47c444e7974f65d26709d4c
Talevin12 <talevin2000@gmail.com>
2023-09-12 23:20:03 +0300

    Add NBA nav bar

---
+++ web-application-front-end/src/components/leagueNavBar/NBANavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './NBANavBar.css';
+import NBAImage from "../../assests/nba_logo.png"
+
+const NBANavBar = () => {
+
+
+    return (
+        <>
+            <div className="NBANavBar">
+                <img className='NBAImage' src={NBAImage} alt="NBA logo" />
+                <ul>
+                    <li className='NBANavBarMenuTextStyle'>Games</li>
+                    <li className='NBANavBarMenuTextStyle'>Teams</li>
+                    <li className='NBANavBarMenuTextStyle'>Players</li>
+                    <li className='NBANavBarMenuTextStyle'>Injuries</li>
+                    <li className='NBANavBarMenuTextStyle'>Fantasy</li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default NBANavBar;
--- web-application-front-end/src/components/topBar/topBar.jsx
+++ web-application-front-end/src/components/topBar/topBar.jsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import './topbar.css'; 
-import PremierLeagueNavBar from "../leagueNavBar/premierLeagueNavBar"
+import React, { useState } from 'react';
+import './topbar.css';
+import PremierLeagueNavBar from "../leagueNavBar/premierLeagueNavBar";
+import NBANavBar from "../leagueNavBar/NBANavBar";
 import OTLogoImage from "../../assests/OTFantasyLogo.png"
 
 const TopBar = () => {
-    
+    const [currentLeague, setCurrentLeague] = useState("PremierLeague"); // Initialize with the Premier League
+
+    const handleLeagueClick = (league) => {
+        setCurrentLeague(league);
+    }
 
     return (
         <>
-        <div className="top-bar">
-            <div className="logo">
-            <img className='logoImage' src={OTLogoImage} alt="Fantasy logo" />
+            <div className="top-bar">
+                <div className="logo">
+                    <img className='logoImage' src={OTLogoImage} alt="Fantasy logo" />
+                </div>
+                <div className="topBarMenu">
+                    <ul>
+                        <li className='menuTextStyle' onClick={() => handleLeagueClick("NBA")}>NBA</li>
+                        <li className='menuTextStyle' onClick={() => handleLeagueClick("PremierLeague")}>Premier League</li>
+                        <li className='menuTextStyle'>Fantasy</li>
+                        <span className='separator'></span>
+                        <li className='menuTextStyle'>Home</li>
+                        <li className='menuTextStyle'>About</li>
+                        <li className='menuTextStyle'>Login</li>
+                    </ul>
+                </div>
             </div>
-            <div className="topBarMenu">
-                <ul>
-                    <li className='menuTextStyle'>NBA</li>
-                    <li className='menuTextStyle'>Premier League</li>
-                    <li className='menuTextStyle'>Fantasy</li>
-                    <span className='separator'></span>
-                    <li className='menuTextStyle'>Home</li>
-                    <li className='menuTextStyle'>About</li>
-                    <li className='menuTextStyle'>Login</li>
-                </ul>
-            </div>
-        </div>
-        <PremierLeagueNavBar/>
-    </>
+            {currentLeague === "PremierLeague" ? <PremierLeagueNavBar /> : <NBANavBar />}
+        </>
     );
 }
 
