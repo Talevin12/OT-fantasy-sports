f1a4e2c6d4c34b5786d5991c5ecdf1e7fd32a4b9
Talevin12 <talevin2000@gmail.com>
2023-09-13 21:35:44 +0300

    List of fixtures design

--- web-application-front-end/src/components/fixtures/colorsTeams.jsx
+++ web-application-front-end/src/components/fixtures/colorsTeams.jsx
@@ -1,24 +1,24 @@
 const premierLeagueTeamsColors = [
-    { name: "Arsenal", color: "#EF0107" }, // Red
-    { name: "Aston Villa", color: "#670E36" }, // Claret and Blue
-    { name: "Brentford", color: "#FFD100" }, // Red and Yellow
-    { name: "Brighton & Hove Albion", color: "#0055A4" }, // Blue and White
-    { name: "Burnley", color: "#6C1D45" }, // Claret and Blue
-    { name: "Chelsea", color: "#034694" }, // Blue
-    { name: "Crystal Palace", color: "#1B458F" }, // Blue and Red
-    { name: "Everton", color: "#003399" }, // Blue
-    { name: "Leeds United", color: "#0033A0" }, // Blue and Yellow
-    { name: "Leicester City", color: "#003090" }, // Blue
-    { name: "Liverpool", color: "#E41B23" }, // Red
-    { name: "Manchester City", color: "#6CADDF" }, // Sky Blue
-    { name: "Manchester United", color: "#DA020E" }, // Red
-    { name: "Newcastle United", color: "#000000" }, // Black and White
-    { name: "Norwich City", color: "#00AEEF" }, // Yellow and Green
-    { name: "Southampton", color: "#D71920" }, // Red and White
-    { name: "Tottenham Hotspur", color: "#001C58" }, // Navy Blue
-    { name: "Watford", color: "#FBEE23" }, // Yellow
-    { name: "West Ham United", color: "#7A263A" }, // Claret and Blue
-    { name: "Wolverhampton Wanderers", color: "#FDB913" }, // Gold and Black
+    { "name": "Arsenal", "color": "#DB0007" },
+    { "name": "Aston Villa", "color": "#670E36" },
+    { "name": "Bournemouth", "color": "#C91318" },
+    { "name": "Brentford", "color": "#DB0007" },
+    { "name": "Brighton", "color": "#005DAA" },
+    { "name": "Burnley", "color": "#610C0D" },
+    { "name": "Chelsea", "color": "#034694" },
+    { "name": "Crystal Palace", "color": "#022169" },
+    { "name": "Fulham", "color": "#000000" },
+    { "name": "Liverpool", "color": "#D00027" },
+    { "name": "Luton", "color": "#FFFFFF" },
+    { "name": "Manchester City", "color": "#6CADDF" },
+    { "name": "Manchester United", "color": "#DA0106" },
+    { "name": "Newcastle", "color": "#FFFFFF" },
+    { "name": "Sheffield Utd", "color": "#EE2737" },
+    { "name": "Tottenham", "color": "#132257" },
+    { "name": "West Ham", "color": "#7C2C3B" },
+    { "name": "Wolves", "color": "#FDB913" },
+    { "name": "Nottingham Forest", "color": "#E53233" },
+    { "name": "Everton", "color": "#274488" }
 ];
 
 export default premierLeagueTeamsColors;
