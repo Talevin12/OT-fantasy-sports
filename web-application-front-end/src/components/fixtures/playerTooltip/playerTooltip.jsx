fc1680a045cb06b823296c140bf3d2a167867da0
OrAmram100 <63103357+OrAmram100@users.noreply.github.com>
2023-09-20 23:48:08 +0300

    Added match lineups with scale image issue

---
+++ web-application-front-end/src/components/fixtures/playerTooltip/playerTooltip.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./playerTooltip.css"
+
+const PlayerTooltip = ({ player }) => {
+    return (
+        <div className="tooltip">
+            <img src={`https://media.api-sports.io/football/players/${player.player.id}.png`} alt={player.player.name} />
+            <div>{`Name: ${player.player.name}`}</div>
+            <div>{`Position: ${player.player.pos}`}</div>
+            <div>{`Number: ${player.player.number}`}</div>
+        </div>
+    );
+};
+
+export default PlayerTooltip;
